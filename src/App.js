19d4c80e9c0dd082a97114f6699ac0c0395ae0ff
EGollahon <horselover@stellarwerx.com>
2022-02-28 17:09:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState , useEffect } from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const [boxArray, setBoxArray] = useState([]);
+    const [boxColor, setBoxColor] = useState('blue');
+    const colorArray = ['blue', 'purple', 'green', 'red'];
+
+    const boxStyle = {
+        width: '50px',
+        height: '50px',
+        backgroundColor: boxColor,
+        marginBottom: '10px'
+    }
+
+    const boxEles = boxArray.map((obj, index) =>
+        <div style={boxStyle} key={index} className="box"></div>
+    )
+
+    function addCounter() {
+        setCounter(counter + 1);
+    }
+
+    function resetCounter() {
+        setCounter(0);
+    }
+
+    useEffect(() => {
+        if (counter === 0) {
+            setBoxArray([]);
+        }
+        if (counter % 2 !== 0) {
+            let newEntry = [boxArray.length];
+            setBoxArray(boxArray.concat(newEntry));
+        }
+        if (counter % 5 === 0) {
+            let newColor = boxColor;
+            while (newColor === boxColor) {
+                let colorIndex = Math.floor(Math.random() * (4 - 0) + 0);
+                newColor = colorArray[colorIndex];
+            }
+            setBoxColor(newColor);
+        }
+    }, [counter])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <header>
+            <div className="button-container">
+                <div onClick={addCounter} id="counter-button">Increase by 1</div>
+                <div onClick={resetCounter} id="reset-button">Reset</div>
+            </div>
+            <div id="counter">{counter}</div>
+        </header>
+        <main>
+            {boxEles}
+        </main>
     </div>
   );
 }
